f0eee9b38172225bdc3355bd5efc3a4a3ba2bdf5
Matt Sayre <mlsayre@gmail.com>
2013-06-14 00:25:20 -0700

    final version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class String
   def shuffle
     self.split('').shuffle.join
   end
+
+  def scrabble
+  	self.downcase
+  	self.split("").map {|s| s.gsub(/[aeilnorstu]/, '1').gsub(/[dg]/, '2').gsub(/[bcmp]/, 
+  		'3').gsub(/[fhvwy]/, '4').gsub(/[k]/, '5').gsub(/[jx]/, 
+  		'8').gsub(/[qz]/, '10')}.map(&:to_i).reduce(:+)
+  
+  end
+
 end
